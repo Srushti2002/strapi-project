49963de9fc174a404247487a06cb5e22b94e57f2
Srushti <srushtideshmukh54@gmail.com>
2024-03-23 14:19:52 +0530

    asdfgh

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,5 @@
 module.exports = ({ env }) => ({
   
-    url: "/", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
-    serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
-
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
